7f5eea2de68208c9d61912d8e1b00e2333ccc9c3
Gabriel Meza Prada <standbyg2@gmail.com>
2025-09-03 17:45:39 -0500

    probably last commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://backendfundae.onrender.com',
-  openAIApiKey: '',
+  openAIApiKey: '${OPENAI_API_KEY}',
   apiUrlApi: 'https://api.openai.com',
 };
